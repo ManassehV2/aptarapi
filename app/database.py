0450f36d5fca1dc2d0e2388191e53b7f13ad19a2
Minase Mengistu <minase.mengistu@abo.fi>
2024-09-15 13:28:32 +0200

    Dockerize the mysql server

--- app/database.py
+++ app/database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+#default to sqllite in memory for testing if DB_CONNECTION_STRING empty
 SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONNECTION_STRING", "sqlite:///:memory:")
 
 
